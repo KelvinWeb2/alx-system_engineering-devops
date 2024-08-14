7fd72efcd2e4c25fc5985935d2b42be2cadeb6db
Your Name <you@example.com>
2024-08-13 18:11:18 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Android 11; Mobile; TECNO LE6) "
+        "User-Agent": "Mozilla/5.0 (Android 11; Mobile; TECNO LE6)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
